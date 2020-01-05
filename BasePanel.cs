ec3872411f34ee7322566b61e7e606174debc0e3
yangfeng gao <gaoyangfeng@hotmail.com>
2020-01-05 17:20:52 +0800

    添加项目文件。

---
+++ BasePanel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace AutoStudio
+{
+    public partial class BasePanel : DockContent
+    {
+        public DockWindow leftSon;
+        public BasePanel()
+        {
+            
+            InitializeComponent();
+            
+            
+            
+      }
+    }
+
+    public class Tree : BasePanel
+    {
+        public Tree(DockPanel master)
+        {
+            Show(master,DockState.DockLeft);
+            
+        }
+    }
+
+    public class Edictor : BasePanel
+    {
+        public Edictor(DockPanel master)
+        {
+            Show(master, DockState.Document);
+        }
+    }
+
+}
---
+++ EplanForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace AutoStudio
+{
+    public partial class EplanForm : Form
+    {
+        public DockContent tree;
+        public DockContent editor;
+        public EplanForm()
+        {
+            InitializeComponent();
+            InitLayOut();
+        }
+
+        public void InitLayOut()
+        {
+           
+            tree = new Tree(dockPanel1);
+            editor = new Edictor(dockPanel1);
+     
+        }
+    }
+}
